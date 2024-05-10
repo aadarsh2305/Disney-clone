75f3da6c47387bf47a299bcd978a2c35ea13a270
Aadarsh Gupta <Aadarsh.Gupta@Prolifics.com>
2024-05-10 13:17:03 +0530

    added basic components

--- disney-clone/src/App.jsx
+++ disney-clone/src/App.jsx
@@ -1,49 +1,13 @@
-import logo from "./../src/assets/Images/logo.png";
+
 import "./App.css";
 import Header from "./components/Header";
-import {
-  HiHome,
-  HiMagnifyingGlass,
-  HiStar,
-  HiPlayCircle,
-  HiTv,
-} from "react-icons/hi2";
-import { HiPlus, HiDotsVertical } from "react-icons/hi";
-import HeaderItem from "./components/HeaderItem";
+
 
 function App() {
-  const menu = [
-    {
-      name: "HOME",
-      icon: HiHome,
-    },
-    {
-      name: "SEARCH",
-      icon: HiMagnifyingGlass,
-    },
-    {
-      name: "WATCH LIST",
-      icon: HiPlus,
-    },
-    {
-      name: "ORIGINALS",
-      icon: HiStar,
-    },
-    {
-      name: "MOVIES",
-      icon: HiPlayCircle,
-    },
-    {
-      name: "SERIES",
-      icon: HiTv,
-    },
-  ];
+  
   return (
     <div className="">
-      <img src={logo} className="w-[80px] object-cover" />
-      {menu.map((item)=>(
-        <HeaderItem name={item.name} Icon={item.icon}/>
-      ))}
+      <Header />
     </div>
   );
 }
--- disney-clone/src/components/HeaderItem.jsx
+++ disney-clone/src/components/HeaderItem.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function HeaderItem(props) {
   return (
-    <div className='text-white'>
+    <div className="text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8">
       <props.Icon />
-      <h2>{props.name}</h2>
+      <h2 className="">{props.name}</h2>
     </div>
-  )
+  );
 }
 
-export default HeaderItem
+export default HeaderItem;
