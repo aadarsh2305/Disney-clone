76e8c4104c731d369f834c5326df3f3c9c328db3
Aadarsh Gupta <aadarshlohari91111@gmail.com>
2024-05-12 03:16:46 +0530

    added css to ProductionHouse.jsx file

--- disney-clone/src/components/ProductionHouse.jsx
+++ disney-clone/src/components/ProductionHouse.jsx
@@ -12,42 +12,45 @@ import pixarV from './../assets/Videos/pixar.mp4'
 import starWarV from './../assets/Videos/star-wars.mp4'
 
 function ProductionHouse() {
-    const productionHouseList=[
+    const productionHouseList = [
         {
-            id:1,
-            image:disney,
-            video:disneyV
+            id: 1,
+            image: disney,
+            video: disneyV
         },
         {
-            id:2,
-            image:marvel,
-            video:marvelV
+            id: 2,
+            image: marvel,
+            video: marvelV
         },
         {
-            id:3,
-            image:nationalG,
-            video:nationalGeographicV
+            id: 3,
+            image: nationalG,
+            video: nationalGeographicV
         },
         {
-            id:4,
-            image:pixar,
-            video:pixarV
+            id: 4,
+            image: pixar,
+            video: pixarV
         },
         {
-            id:5,
-            image:starwar,
-            video:starWarV
+            id: 5,
+            image: starwar,
+            video: starWarV
         },
     ]
-  return (
-    <div className='flex gap-5 p-2 px-5 md:px-16'>
-        {productionHouseList.map((item)=>(
-            <div className='border-[2px] border-gray-600 rounded-lg'>
-                <img src={item.image} className='w-full'/>
-            </div>
-        ))}
-    </div>
-  )
+    return (
+        <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16'>
+            {productionHouseList.map((item) => (
+                <div className='border-[2px] border-gray-600 rounded-lg hover:scale-105 transition-all duration-300 ease-out cursor-pointer relative shadow-xl shadow-gray-800 '>
+
+                    <video src={item.video} autoPlay loop playsInline className='absolute top-0 rounded-md z-0 opacity-0 hover:opacity-50' />
+                    <img src={item.image} className='w-full z-[1]' />
+
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default ProductionHouse
