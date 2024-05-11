f392d10b3a95cd8b423ac630d4d6fddce03dc84d
Aadarsh Gupta <aadarshlohari91111@gmail.com>
2024-05-11 18:05:58 +0530

    improved css and used axios

---
+++ disney-clone/src/Services/GlobalApi.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const movieBaseUrl="https://api.themoviedb.org/3"
+const api_key='d755cfe79dbc8447b1adaa452fc8687d'
+//https://api.themoviedb.org/3/trending/all/day?api_key=d755cfe79dbc8447b1adaa452fc8687d
+
+const getTrendingVideos=axios.get(this.movieBaseUrl+"trending/all/day?api_key="+api_key)
+
+export default {
+    getTrendingVideos
+}
--- disney-clone/src/components/Header.jsx
+++ disney-clone/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderItem from "./HeaderItem";
 import logo from "../assets/Images/logo.png";
 import {
@@ -11,6 +11,8 @@ import {
 import { HiPlus, HiDotsVertical } from "react-icons/hi";
 
 function Header() {
+  const [toggle, setToggle] = useState(true);
+
   const menu = [
     {
       name: "HOME",
@@ -41,28 +43,28 @@ function Header() {
     <div className="flex items-center gap-8 justify-between p-5">
       <div className="flex gap-8 items-center">
         <img src={logo} className="w-[80px] object-cover" />
-        
+
         <div className="hidden md:flex gap-8">
           {menu.map((item) => (
             <HeaderItem name={item.name} Icon={item.icon} />
           ))}
         </div>
-        
+
         <div className="flex md:hidden gap-5">
-          {menu.map((item,index) =>index<3&&(
+          {menu.map((item, index) => index < 3 && (
             <HeaderItem name={''} Icon={item.icon} />
           ))}
         </div>
 
-        <div className="md:hidden">
-            <HeaderItem name={''} Icon={HiDotsVertical} />
-            <div className="absolute mt-3 bg-[#121212] border-[1px] p-3">
-            {menu.map((item,index) =>index>3&&(
-            <HeaderItem name={item.name} Icon={item.icon} />
-          ))}
-            </div>
+        <div className="md:hidden" onClick={()=>setToggle(!toggle)}>
+          <HeaderItem name={''} Icon={HiDotsVertical} />
+          {toggle ? <div className="absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 pb-4">
+            {menu.map((item, index) => index > 2 && (
+              <HeaderItem name={item.name} Icon={item.icon} />
+            ))}
+          </div> : null}
         </div>
-      
+
       </div>
       <img
         className="w-[40px] rounded-full"
