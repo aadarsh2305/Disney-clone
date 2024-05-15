607fd66531db8c18ca3bae01cfb7be87c1fc1f34
Aadarsh Gupta <Aadarsh.Gupta@Prolifics.com>
2024-05-15 17:49:32 +0530

    added functions in moviecard and movielist

--- disney-clone/src/components/MovieList.jsx
+++ disney-clone/src/components/MovieList.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import GlobalApi from '../Services/GlobalApi'
+import { useEffect ,useState} from 'react';
+import MovieCard from './MovieCard';
 
 function MovieList({genreId}) {
+
+    const [MovieList, setMovieList] = useState([]);
+
+    useEffect(() => {
+      getMovieByGenreId();
+    }, [])
+    
+
+    const getMovieByGenreId=()=>{
+        GlobalApi.getMovieByGenreId(genreId).then(resp=>{
+            console.log(resp.data.results)
+            setMovieList(resp.data.results)
+        })
+    }
   return (
-    <div>
-      MovieList
+    <div className='flex overflow-x-auto gap-8 scrollbar-hide pt-5 px-3'>
+      {MovieList.map((item,index)=>(
+        <MovieCard movie={item}/>
+      ))}
     </div>
   )
 }
