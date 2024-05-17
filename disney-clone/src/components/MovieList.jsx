1562c5d3f868e2f7075793b74e7fe4ce6f2fb3d1
Aadarsh Gupta <Aadarsh.Gupta@Prolifics.com>
2024-05-17 11:02:32 +0530

    added one more componet HrMovieCard.jsx with some better css

--- disney-clone/src/components/MovieList.jsx
+++ disney-clone/src/components/MovieList.jsx
@@ -3,8 +3,9 @@ import GlobalApi from "../Services/GlobalApi";
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
+import HrMovieCard from "./HrMovieCard";
 
-function MovieList({ genreId }) {
+function MovieList({ genreId, index_ }) {
   const [MovieList, setMovieList] = useState([]);
   const elementRef = useRef(null);
 
@@ -30,23 +31,27 @@ function MovieList({ genreId }) {
        {/* This is the Left slider for Genre  */}
       <IoChevronBackOutline
         onClick={() => slideLeft(elementRef.current)}
-        className='text-[50px] text-white
+        className={`text-[50px] text-white
            p-2 z-10 cursor-pointer 
-            hidden md:block absolute mt-[150px]'
+            hidden md:block absolute 
+            ${index_%3==0?'mt-[80px]':'mt-[150px]'}`}
       />
 
       <div ref={elementRef} className="flex overflow-x-auto gap-8 scrollbar-hide scroll-smooth pt-5 px-3 pb-5">
         {MovieList.map((item, index) => (
-          <MovieCard movie={item} />
+            <>
+           {index_%3==0? <HrMovieCard movie={item}/> : <MovieCard movie={item}/>}
+           </>
         ))}
       </div>
 
       {/* This is for the Right slider for Genre */}
       <IoChevronForwardOutline
         onClick={() => slideRight(elementRef.current)}
-        className='text-[50px] text-white hidden md:block
+        className={`text-[50px] text-white hidden md:block
            p-2 cursor-pointer z-10 top-0
-            absolute right-0 mt-[150px]'
+            absolute right-0
+            ${index_%3==0?'mt-[80px]':'mt-[150px]'}`}
       />
     </div>
   );
--- disney-clone/src/components/Slider.jsx
+++ disney-clone/src/components/Slider.jsx
@@ -35,15 +35,15 @@ function Slider() {
 
     return (
         <div >
-            <HiChevronLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
+            <HiChevronLeft className="hidden md:block text-white text-[40px] absolute mx-8 mt-[300px]  cursor-pointer"
                 onClick={() => sliderLeft(elementRef.current)} />
-            <HiChevronRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
+            <HiChevronRight className="hidden md:block text-white text-[40px] absolute mx-8 mt-[300px] cursor-pointer right-0"
                 onClick={() => sliderRight(elementRef.current)} />
             
             <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth' ref={elementRef}>
                 {movieList.map((item, index) => (
                     <img src={IMAGE_BASE_URL + item.backdrop_path}
-                        className='min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in' />
+                        className='min-w-full md:h-[600px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in' />
                 ))}
             </div>
         </div>
